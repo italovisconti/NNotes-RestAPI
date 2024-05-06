7fbaec34c58ec8c066d5ea91be3e8402a929ab2f
jerojash <javiereduardorh@gmail.com>
2024-05-06 08:31:29 -0400

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,13 @@ import db from "./mongo"
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: [
+      'http://localhost:3000',
+      'https://localhost:3000',
+    ],
+    credentials: false,
+  }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
